fd2e9cfbdc948f148689384cdf00a2b623f12668
Dhairya <dhairyasehgal2307@gmail.com>
2022-06-29 17:18:11 +0530

    make it better

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
-          <Header/>
+          <Header />
           <Routes>
-            <Route path='/' element={<Dashboard />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-             <Route path='/register' element={<Register/>}></Route>
-       </Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
         </div>
-        </Router>
-      </>
-      
-  );
+      </Router>
+      <ToastContainer />
+    </>
+  )
 }
 
-export default App;
+export default App
